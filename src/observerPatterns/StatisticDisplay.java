7e574f4995004637096ccf869f4bdf659f3510e5
xyx <707997228@qq.com>
2020-11-28 13:53:08 +0800

    观察者模式

--- src/observerPatterns/StatisticDisplay.java
+++ src/observerPatterns/StatisticDisplay.java
@@ -7,8 +7,8 @@ public class StatisticDisplay implements Observer, DisplayElements {
     Observable observable;
     private int times;
     private float sumTem;
-    private float maxTem;
-    private float minTem;
+    private float maxTem = Float.MIN_VALUE;
+    private float minTem = Float.MAX_VALUE;
 
     public StatisticDisplay(Observable observable) {
         this.observable = observable;
@@ -17,11 +17,23 @@ public class StatisticDisplay implements Observer, DisplayElements {
 
     @Override
     public void update(Observable o, Object arg) {
-
+        if (o instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData) o;
+            float tem = weatherData.getTemperature();
+            times += 1;
+            sumTem += tem;
+            if (tem > maxTem) {
+                maxTem = tem;
+            }
+            if (tem < minTem) {
+                minTem = tem;
+            }
+            display();
+        }
     }
 
     @Override
     public void display() {
-
+        System.out.printf("Avg/Max/Min temperature = %.1f/%.1f/%.1f %n", sumTem/times, maxTem, minTem);
     }
 }
